be174b7bf8c6e8a16b782ab810d0bfb2b158d557
Taeila92 <kim920125@naver.com>
2021-04-30 22:00:29 +0900

    width1024 responsive update

--- js/responsive.js
+++ js/responsive.js
@@ -36,8 +36,8 @@ const btnEl1 = document.querySelector('.culture .btn.culture1')
 const btnEl2 = document.querySelector('.culture .btn.culture2')
 
 btnEl1.addEventListener('click', function(){
-  document.querySelector('.culture .inner').style.transform = 'translateX(0vw)'
+  document.querySelector('.inner .culture-items').style.transform = 'translateX(0vw)'
 })
 btnEl2.addEventListener('click', function(){
-  document.querySelector('.culture .inner').style.transform = 'translateX(-100vw)'
+  document.querySelector('.inner .culture-items').style.transform = 'translateX(-100vw)'
 })
